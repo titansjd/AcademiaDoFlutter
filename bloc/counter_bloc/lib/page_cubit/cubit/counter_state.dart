c0bc00831b835cb0eba1cf9c60bb2645c565f0e7
Diego Lopes <titansjd@gmail.com>
2024-11-11 17:50:21 -0300

    commit inicial projeto contact_bloc

--- bloc/counter_bloc/lib/page_cubit/cubit/counter_state.dart
+++ bloc/counter_bloc/lib/page_cubit/cubit/counter_state.dart
@@ -3,6 +3,16 @@ part of 'counter_cubit.dart';
 abstract class CounterState {
   final int counter;
   const CounterState(this.counter);
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is CounterState && other.counter == counter;
+  }
+
+  @override
+  int get hashCode => counter.hashCode;
 }
 
 class CounterStateInitial extends CounterState {
