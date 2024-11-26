7fadab24bd2e9b3e211ba7348ba95e1f1c763b00
Diego Lopes <titansjd@gmail.com>
2024-11-26 16:56:03 -0300

    aula 14 - Implementando busca de contatos

--- bloc/contact_bloc/lib/features/contacts/list/contacts_list_page.dart
+++ bloc/contact_bloc/lib/features/contacts/list/contacts_list_page.dart
@@ -1,4 +1,7 @@
+import 'package:contact_bloc/features/contacts/list/bloc/contact_list_bloc.dart';
+import 'package:contact_bloc/models/contact_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ContactsListPage extends StatelessWidget {
   const ContactsListPage({super.key});
@@ -7,9 +10,40 @@ class ContactsListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(''),
+        title: const Text('Contact List'),
       ),
-      body: Container(),
+      body: CustomScrollView(
+        slivers: [
+          SliverFillRemaining(
+            child: Column(
+              children: [
+                BlocSelector<ContactListBloc, ContactListState,
+                    List<ContactModel>>(
+                  selector: (state) {
+                    return state.maybeWhen(
+                      data: (contacts) => contacts,
+                      orElse: () => [],
+                    );
+                  },
+                  builder: (_, contacts) {
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: contacts.length,
+                      itemBuilder: (context, index) {
+                        final contact = contacts[index];
+                        return ListTile(
+                          title: Text(contact.name),
+                          subtitle: Text(contact.email),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- bloc/contact_bloc/lib/main.dart
+++ bloc/contact_bloc/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:contact_bloc/features/bloc_example/bloc_example_page.dart';
 import 'package:contact_bloc/features/bloc_example/bloc_freezed/example_freezed_bloc.dart';
+import 'package:contact_bloc/features/contacts/list/bloc/contact_list_bloc.dart';
 import 'package:contact_bloc/features/contacts/list/contacts_list_page.dart';
+import 'package:contact_bloc/repositories/contacts_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -17,25 +19,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: '/home',
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return RepositoryProvider(
+      create: (context) => ContactsRepository(),
+      child: MaterialApp(
+        initialRoute: '/home',
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        routes: {
+          '/home': (_) => const HomePage(),
+          '/bloc/example/': (_) => BlocProvider(
+              create: (_) => ExampleBloc()..add(ExampleFindNameEvent()),
+              child: const BlocExamplePage()),
+          '/bloc/example/freezed': (context) => BlocProvider(
+                create: (_) => ExampleFreezedBloc()
+                  ..add(const ExampleFreezedEvent.findNames()),
+                child: const BlocFreezedExamplePage(),
+              ),
+          '/contacts/list': (context) => BlocProvider(
+                create: (_) => ContactListBloc(
+                    repository: context.read<ContactsRepository>())
+                  ..add(
+                    ContactListEvent.findAll(),
+                  ),
+                child: ContactsListPage(),
+              ),
+        },
       ),
-      routes: {
-        '/home': (_) => const HomePage(),
-        '/bloc/example/': (_) => BlocProvider(
-            create: (_) => ExampleBloc()..add(ExampleFindNameEvent()),
-            child: const BlocExamplePage()),
-        '/bloc/example/freezed': (context) => BlocProvider(
-              create: (context) => ExampleFreezedBloc()
-                ..add(const ExampleFreezedEvent.findNames()),
-              child: const BlocFreezedExamplePage(),
-            ),
-        '/contacts/list': (context) => ContactsListPage(),
-      },
     );
   }
 }
