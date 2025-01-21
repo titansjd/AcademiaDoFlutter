6f134e3c52bf2c1e908057181b8ee4e7cfa21818
Diego Lopes <diego.jlopes@terceiroslinx.com.br>
2025-01-21 16:31:06 -0300

    aula 16 - Adicionando pull refresh

--- bloc/contact_bloc/lib/main.dart
+++ bloc/contact_bloc/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:contact_bloc/features/bloc_example/bloc_example_page.dart';
 import 'package:contact_bloc/features/bloc_example/bloc_freezed/example_freezed_bloc.dart';
 import 'package:contact_bloc/features/contacts/list/bloc/contact_list_bloc.dart';
 import 'package:contact_bloc/features/contacts/list/contacts_list_page.dart';
+import 'package:contact_bloc/features/contacts/register/contact_register_page.dart';
+import 'package:contact_bloc/features/contacts/update/contact_update_page.dart';
 import 'package:contact_bloc/repositories/contacts_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -46,6 +48,8 @@ class MyApp extends StatelessWidget {
                   ),
                 child: ContactsListPage(),
               ),
+          '/contacts/register': (context) => const ContactRegisterPage(),
+          '/contacts/update': (context) => const ContactUpdatePage(),
         },
       ),
     );
